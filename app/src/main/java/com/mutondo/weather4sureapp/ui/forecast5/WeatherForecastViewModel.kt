567e7e20d74e98f510a6c179c51a52543d21a644
benmutondo <ben_mutondo@hotmail.com>
2023-09-29 09:01:44 +0200

    weather forecast viewmodel

---
+++ app/src/main/java/com/mutondo/weather4sureapp/ui/forecast5/WeatherForecastViewModel.kt
@@ -0,0 +1,33 @@
+package com.mutondo.weather4sureapp.ui.forecast5
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.mutondo.weather4sureapp.data.Resource
+import com.mutondo.weather4sureapp.data.ResourceStatus
+import com.mutondo.weather4sureapp.domain.models.Forecast
+import com.mutondo.weather4sureapp.domain.repositories.WeatherRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class WeatherForecastViewModel @Inject constructor(
+    private val weatherRepository: WeatherRepository
+) : ViewModel() {
+
+    fun getWeatherForecast(latitude: String, longitude: String, appId: String)
+    : LiveData<Resource<List<Forecast>>> {
+        return liveData {
+            val resource = weatherRepository.getForecast5Data(latitude, longitude, appId)
+
+            when (resource.status) {
+                ResourceStatus.ERROR -> {
+                    emit(Resource.error(resource.errorType, null))
+                }
+                ResourceStatus.SUCCESS -> {
+                    emit(resource)
+                }
+            }
+        }
+    }
+}
