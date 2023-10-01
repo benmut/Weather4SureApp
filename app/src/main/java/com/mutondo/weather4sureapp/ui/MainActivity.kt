310f92a76edc7366e1ea663d9ab54fadc40e4ec2
benmutondo <ben_mutondo@hotmail.com>
2023-10-01 15:01:39 +0200

    add menu option to make temperature unit configurable

--- app/src/main/java/com/mutondo/weather4sureapp/ui/MainActivity.kt
+++ app/src/main/java/com/mutondo/weather4sureapp/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.mutondo.weather4sureapp.ui
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.commit
+import androidx.preference.PreferenceManager
 import com.mutondo.weather4sureapp.R
 import com.mutondo.weather4sureapp.databinding.ActivityMainBinding
 import com.mutondo.weather4sureapp.ui.map.MapFragment
@@ -25,6 +29,50 @@ class MainActivity : BaseActivity() {
                 setReorderingAllowed(true)
                 add(R.id.fragment_container, mapFragment, MapFragment.TAG)
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_settings -> {
+                showTemperatureSettings()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+    private var checkedItem = 0
+    private fun showTemperatureSettings() {
+        val temperatureUnits = arrayOf("Celsius", "Fahrenheit")
+
+        AlertDialog.Builder(this)
+            .setTitle("Temperature Unit")
+            .setPositiveButton("Save") { dialog, which ->
+                saveTemperatureUnit(temperatureUnits[checkedItem])
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+
+            }
+            .setSingleChoiceItems(temperatureUnits, checkedItem) { dialog, which ->
+                checkedItem = which
+            }
+            .create()
+            .show()
+    }
+
+    private fun saveTemperatureUnit(unit: String) {
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(this)
+
+        with (sharedPref.edit()) {
+            putString(getString(R.string.temperature_unit_key), unit)
+            apply()
         }
     }
 }
