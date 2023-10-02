aed0ca65097683b2b2991e6b00cfe4f9a3e067a9
benmutondo <ben_mutondo@hotmail.com>
2023-10-02 10:28:45 +0200

    update map fragment to save favorite location

--- app/src/main/java/com/mutondo/weather4sureapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/mutondo/weather4sureapp/ui/map/MapFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,17 +22,25 @@ import com.google.android.libraries.places.widget.listener.PlaceSelectionListene
 import com.mutondo.weather4sureapp.BuildConfig
 import com.mutondo.weather4sureapp.R
 import com.mutondo.weather4sureapp.databinding.FragmentMapBinding
+import com.mutondo.weather4sureapp.domain.models.FavoriteLocation
 import com.mutondo.weather4sureapp.ui.BaseFragment
+import com.mutondo.weather4sureapp.ui.FavoriteLocationViewModel
 import com.mutondo.weather4sureapp.ui.forecast5.WeatherForecastFragment
 import com.mutondo.weather4sureapp.utils.AppUtils.Companion.showFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MapFragment : BaseFragment(),
-    OnMapReadyCallback,
-    OnMapLongClickListener,
-    OnInfoWindowLongClickListener {
+@AndroidEntryPoint
+class MapFragment : BaseFragment(), OnMapReadyCallback, OnMapLongClickListener, OnInfoWindowLongClickListener {
 
     private var binding: FragmentMapBinding? = null
     lateinit var map: GoogleMap
+
+    private val markers = mutableListOf<Marker>()
+
+    @Inject
+    lateinit var viewModel: FavoriteLocationViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
@@ -75,8 +84,18 @@ class MapFragment : BaseFragment(),
             override fun onPlaceSelected(place: Place) {
                 Log.i(TAG, "Place: ${place.name}, LatLng: ${place.latLng}" )
                 addMarker(map, place.latLng!!)
+
+                saveFavoriteLocation(place.name!!, place.latLng!!.latitude, place.latLng!!.longitude)
             }
         }
+
+    private fun saveFavoriteLocation(name: String, latitude: Double, longitude: Double) {
+        lifecycleScope.launch {
+
+        val favoriteLocation = FavoriteLocation(name, latitude, longitude)
+            viewModel.saveFavoriteLocation(favoriteLocation)
+        }
+    }
 
     override fun onResume() {
         super.onResume()
@@ -109,7 +128,9 @@ class MapFragment : BaseFragment(),
 
     override fun onMapLongClick(point: LatLng) {
         val latLng = LatLng(point.latitude, point.longitude)
-        addMarker(map, latLng)
+
+        removeMarkers(markers)
+        addMarker(map, latLng)?.let { markers.add(it) }
     }
 
     override fun onInfoWindowLongClick(marker: Marker) {
@@ -119,15 +140,21 @@ class MapFragment : BaseFragment(),
         showFragment(requireActivity(), R.id.fragment_container, weatherForecastFragment, WeatherForecastFragment.TAG)
     }
 
-    private fun addMarker(map: GoogleMap, latLng: LatLng) {
-        map.addMarker(
+    private fun addMarker(map: GoogleMap, latLng: LatLng): Marker? {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_LEVEL))
+
+        return map.addMarker(
             MarkerOptions()
                 .position(latLng)
                 .title("You are here")
                 .snippet("Lat: ${latLng.latitude}; Lng: ${latLng.longitude}")
         )
+    }
 
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, ZOOM_LEVEL))
+    private fun removeMarkers(list: MutableList<Marker>) {
+        for (marker: Marker in list) {
+            marker.remove()
+        }
     }
 
     companion object {
