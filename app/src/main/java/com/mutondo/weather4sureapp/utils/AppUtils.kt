e42f7c9132350aec49641ef0779718de0e31ddd8
benmutondo <ben_mutondo@hotmail.com>
2023-10-02 14:08:29 +0200

    add letter C and F to degree symbol to better differentiate the temperature unit

--- app/src/main/java/com/mutondo/weather4sureapp/utils/AppUtils.kt
+++ app/src/main/java/com/mutondo/weather4sureapp/utils/AppUtils.kt
@@ -10,11 +10,11 @@ class AppUtils {
     companion object {
 
         fun convertKelvinToCelsius(kelvin: Float): String {
-            return (kelvin - 273.15f).roundToInt().toString() + "\u00B0"
+            return (kelvin - 273.15f).roundToInt().toString() + "\u00B0C"
         }
 
         fun convertKelvinToFahrenheit(kelvin: Float): String {
-            return ((kelvin - 273.15f) * 9/5 + 32).roundToInt().toString() + "\u00B0"
+            return ((kelvin - 273.15f) * 9/5 + 32).roundToInt().toString() + "\u00B0F"
         }
 
         fun showFragment(activity: FragmentActivity, @IdRes containerViewId: Int, fragment: Fragment, tag: String) {
